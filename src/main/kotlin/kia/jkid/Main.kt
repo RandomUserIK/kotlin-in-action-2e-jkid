16f3d0b9a0e42c2d579fa9c71157fc6b2a4ca174
RandomUserIK <kovac.ivan97@icloud.com>
2025-09-14 18:30:55 +0200

    feat(reflection basics): add basic reflection implementation

--- src/main/kotlin/kia/jkid/Main.kt
+++ src/main/kotlin/kia/jkid/Main.kt
@@ -1,9 +1,46 @@
 package kia.jkid
 
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.reflect.full.memberProperties
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+class Person(val name: String, val age: Int)
+
+interface User {
+	val username: String
+}
+
+fun foo(x: Int) = println(x)
+
+fun sum(x: Int, y: Int) = x + y
+
+var counter = 0
+
+fun main(args: Array<String>) {
+	val person = Person("Alice", 29)
+	val kClass = person::class
+
+	println(kClass.simpleName)
+	println("Member properties:")
+	kClass.memberProperties.forEach { println(it) }
+
+	val anonymous = object : User {
+		override val username: String
+			get() = "anonymous"
+	}
+
+	println(anonymous::class.simpleName) // null - since it is an anonymous object
+
+	val kFunction = ::foo
+	kFunction.call(42) // 42 - does not prevent to call with incorrect number of parameters
+	kFunction(43) // 43
+
+	val kFunction2 = ::sum
+	println(kFunction2.invoke(1, 2) + kFunction2.invoke(3, 4)) // 10
+	// kFunction2(1) // error - no value passed for parameter p2
+
+	val kProperty = ::counter
+	kProperty.setter.call(21)
+	println(kProperty.get()) // 21
+
+	val memberProperty = Person::age
+	println(memberProperty.get(person)) // 29
 }
